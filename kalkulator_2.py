397aa9c71cd41128e896021c2e0e55af7092a73d
Rafał Raźny <rafal-razny@wp.pl>
2022-01-17 11:37:21 +0100

    Add dictionary kalkulator_2.py

--- kalkulator_2.py
+++ kalkulator_2.py
@@ -1,15 +1,37 @@
 import logging
 logging.basicConfig(format ="",level=logging.DEBUG)
-choice = str(input ("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
 
-a = int(input("Podaj składnik 1: "))
-b = int(input("Podaj składnik 2: "))
+choice = int(input ("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+first_arg = int(input("Podaj składnik 1: "))
+second_arg = int(input("Podaj składnik 2: "))
 
+def add (a, b):
+  return a+b
+def substract (a, b):
+  return a-b
+def multiply (a, b):
+  return a*b
+def divide (a, b):
+  return a/b
 
-def calculate(choice):
-  return {
-        "1": logging.info("Dodaję: " + str(a) + " i " + str(b) + " Wynik to " + int(a+b)), 
-        "2": logging.info("Odejmuję: " + str(a) + " i " + str(b) + "/n" + "Wynik to " + int(a-b))
-   }[choice]
+d = {1: add,
+     2: substract,
+     3: multiply,
+     4: divide
+}
 
-calculate(choice)
+function = d[choice]
+result = function(first_arg, second_arg)
+
+if choice == 1:
+  logging.info("Dodaję: " + str(first_arg) + " i " + str(second_arg))
+  print ("Wynik to " + str(result))
+elif choice == 2:
+  logging.info("Odejmuję: " + str(first_arg) + " i " + str(second_arg))
+  print("Wynik to " + str(result))
+elif choice == 3:
+  logging.info("Mnożę: " + str(first_arg) + " i " + str(second_arg))
+  print("Wynik to " + str(result))
+elif choice == 4:
+  logging.info("Dzielę: " + str(first_arg) + " i " + str(second_arg))
+  print("Wynik to " + str(result))
